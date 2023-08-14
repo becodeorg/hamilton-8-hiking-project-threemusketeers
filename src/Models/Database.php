104e4fa1fba4d7cd8a00603f7535916721e88ad2
corentin nys <corentinys@gmail.com>
2023-08-14 10:54:59 +0200

    configuration de base

---
+++ src/Models/Database.php
@@ -0,0 +1,37 @@
+<?php
+declare(strict_types=1);
+
+namespace Models;
+
+use PDO;
+use PDOStatement;
+
+class Database
+{
+    private PDO $pdo;
+
+    public function __construct()
+    {
+        $this->pdo = new PDO(
+            'mysql:host=' . getenv('DB_HOST') . ';dbname=' . getenv('PROD_DATABASE'),
+            getenv('PROD_USERNAME'),
+            getenv('PROD_PASSWORD')
+        );
+
+        $this->pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    }
+
+    public function query(string $query, array $params = []): PDOStatement
+    {
+        $stmt = $this->pdo->prepare($query);
+        $stmt->execute($params);
+
+        return $stmt;
+    }
+
+    public function lastInsertId()
+    {
+        return $this->pdo->lastInsertId();
+    }
+}
