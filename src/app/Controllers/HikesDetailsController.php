c58d5fcc417a9241ae604733830cf6b560f50a1b
CMarchandon <camillemarchandon3@hotmail.fr>
2023-08-17 14:43:05 +0200

    merge with dev

--- src/app/Controllers/HikesDetailsController.php
+++ src/app/Controllers/HikesDetailsController.php
@@ -1,22 +1,23 @@
 <?php
 declare(strict_types=1);
+
 namespace App\Controllers;
-use PDO;
+
 use App\Models\Database;
+use PDO;
 
 class HikesDetailsController extends Database
 {
-    public function hikesDetails()
+    public function hikesDetails($id)
     {
-       
-        $id = (int)$_GET['id'];
         $query = "SELECT * FROM Hikes WHERE id = ?";
-        $stmt = $this->query($query, [$id]);
+        $stmt = $this->getConnection()->prepare($query);
+        $stmt->execute([$id]);
         $hike = $stmt->fetch(PDO::FETCH_ASSOC);
+
         include 'app/Views/layout/header.view.php';
         include 'app/Views/hikesdetails.view.php';
         include 'app/Views/layout/footer.view.php';
-
     }
 }
 
--- src/app/Models/Hikes.php
+++ src/app/Models/Hikes.php
@@ -1,15 +1,25 @@
 <?php 
 namespace App\Models;
+
 use App\Models\Database;
 use PDO;
 
 class Hikes extends Database
 {
+    public function getAllHikes()
+    {
+        $query = "SELECT * FROM Hikes";
+        $stmt = $this->query($query);
+        $hikes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $hikes;
+    }
 
- public function getAllHikes(){
-    $query = "SELECT * FROM Hikes";
-    $stmt = $this->query($query);
-    $hikes = $stmt->fetchAll(PDO::FETCH_ASSOC);
-    return $hikes;
- }
+    public function getHikeById(int $id)
+    {
+        $query = "SELECT * FROM Hikes WHERE id = ?";
+        $stmt = $this->prepare($query);
+        $stmt->execute([$id]);
+        $hike = $stmt->fetch(PDO::FETCH_ASSOC);
+        return $hike;
+    }
 }
