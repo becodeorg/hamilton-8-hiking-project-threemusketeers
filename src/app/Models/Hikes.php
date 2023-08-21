0ff30a3aa496c64909ce279210bd21b4cd02ca89
corentin nys <corentinys@gmail.com>
2023-08-21 11:58:28 +0200

    tags fixed

--- src/app/Models/Hikes.php
+++ src/app/Models/Hikes.php
@@ -10,13 +10,25 @@ use PDO;
 class Hikes extends Database
 {
 
-  public function getAllHikes(){
-    
-      $query = "SELECT * FROM Hikes";
-      $stmt = $this->query($query);
-      $hikes = $stmt->fetchAll(PDO::FETCH_ASSOC);
-      return $hikes;
-  }
+
+
+      public function getAllHikes(){
+          if(is_null($_GET['tag']))
+          {
+              $query = "SELECT * FROM Hikes";
+              $stmt = $this->query($query);
+          }else{
+              $query = "SELECT Tags.name as tagName,Hikes.id,Hikes.name,Hikes.distance,Hikes.duration,Hikes.elevation_gain FROM Hikes 
+    INNER JOIN Hikes_has_tags ON Hikes_has_tags.hike=Hikes.id 
+    INNER JOIN Tags ON Hikes_has_tags.tag = Tags.id
+ WHERE  Tags.name=?" ;
+              $stmt = $this->query($query,[$_GET['tag']]);
+          }
+
+          $hikes = $stmt->fetchAll(PDO::FETCH_ASSOC);
+          return $hikes;
+
+    }
 
   public function createNewHike($name, $distance, $duration, $elevation_gain, $description, $userId, $created_at, $updated_at){
 
